d7e4cec8f811be97dc4755cad633af74aabf12f3
LeviUmr <Lumr@outlook.com.br>
2020-06-10 17:33:50 -0300

    Fiz a tela de login

---
+++ Usuario.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="Usuario.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="row">
+        
+
+        <div class="col s5 red lighten-1 center " id="login">
+            <h1 class="white-text espaço" style="font-family: 'Reem Kufi', sans-serif">Login</h1>
+            <h1 class="white-text" style="font-family: 'Reem Kufi', sans-serif; font-size:1.2rem;">Dados Pessoais</h1>
+            <div class="row">
+                <div class="col s3 "></div>
+                <div>
+                <div class="col s6 center">
+                <input type="text" placeholder="  Insira seu email">
+                <input type="text" placeholder="  Insira sua senha">
+                <a href="" class="white-text rem ">Esqueceu sua senha?</a>
+                <div class="row">
+                    <a class="waves-effect waves-light btn-large white black-text rem btn">Entrar</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+
+        </div>
+
+        <div class="col s7 center " id="cadastro">
+        <h1 class="red-text text-lighten-1 espaço" style="font-family: 'Reem Kufi', sans-serif">Registro</h1>
+        <h1 class="black-text" style="font-family: 'Reem Kufi', sans-serif; font-size:1.2rem">Dados Cadastrais</h1>
+        <div class="row">
+            <div class="col s3"></div>
+            <div class="col s6"><input class="in" type="text" placeholder="  Insira seu email">
+        <input class="in" type="text" placeholder="  Criar sua senha">
+        <input class="in" type="text" placeholder="  Confirme sua senha">
+        <input class="in" type="text" placeholder="  CPF ou CNPJ">
+        <input class="in" type="text" placeholder="  CEP"></div>
+        <div class="row">
+        <a class="waves-effect waves-light btn-large red ligten-1 white-text rem btn">Cadastrar</a>
+        </div>
+        </div>
+        
+        
+       
+
+        </div>
+
+    </div>
+
+</body>
+</html>
