cdb21c8e0346c399f20720bd014ccf2f6e3c1cf2
LeviUmr <Lumr@outlook.com.br>
2020-06-10 12:55:03 -0300

    Primeiro commit

---
+++ Inicial.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicial</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+
+<body>
+  <nav>
+    <nav class="nav-extended yellow">
+      <div class="nav-wrapper">
+        <ul id="nav-mobile" class="right">
+          <li><a href="Inicial.php" class="red-text" style="font-family: Revalia">Produtos</a></li>
+          <li><a href="Carrinho.php" class="red-text" style="font-family: Revalia">Carrinho</a></li>
+          <li><a href="Usuario.php" class="red-text" style="font-family: Revalia">Registrar-se/entrar</a></li>
+        </ul>
+      </div>
+      <div class="nav-header center">
+        <h1 class=" red-text" style="font-family: Revalia">MotorShaft</h1>
+      </div>
+      <div class="nav-content red">
+        <ul class="tabs tabs-transparent">
+          <li class="tab"><a class="yellow-text" href="#test1" style="font-family: Revalia,cursive">Filtrar</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div></div>
+    <ul class="sidenav" id="mobile-demo">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+      <li><a href="collapsible.html">JavaScript</a></li>
+    </ul>
+  </nav>
+
+
+  <div class="row">
+    <div class="col s3">
+      <div class="card medium">
+        <div class="card-image">
+          <img src="" alt="" />
+          <span class="card-title"></span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+
+
+      </div>
+
+
+</body>
+
+</html>
