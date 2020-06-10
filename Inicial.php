dea07ed1059bdeaa5dfcdfdd5c4fe3d96fe6e752
LeviUmr <Lumr@outlook.com.br>
2020-06-10 16:33:18 -0300

    criando as outras telas

--- Inicial.php
+++ Inicial.php
@@ -2,59 +2,159 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inicial</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inicial</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 
-<body>
-  <nav>
-    <nav class="nav-extended yellow">
-      <div class="nav-wrapper">
-        <ul id="nav-mobile" class="right">
-          <li><a href="Inicial.php" class="red-text" style="font-family: Revalia">Produtos</a></li>
-          <li><a href="Carrinho.php" class="red-text" style="font-family: Revalia">Carrinho</a></li>
-          <li><a href="Usuario.php" class="red-text" style="font-family: Revalia">Registrar-se/entrar</a></li>
-        </ul>
-      </div>
-      <div class="nav-header center">
-        <h1 class=" red-text" style="font-family: Revalia">MotorShaft</h1>
-      </div>
-      <div class="nav-content red">
-        <ul class="tabs tabs-transparent">
-          <li class="tab"><a class="yellow-text" href="#test1" style="font-family: Revalia,cursive">Filtrar</a></li>
-        </ul>
-      </div>
-    </nav>
-    <div></div>
-    <ul class="sidenav" id="mobile-demo">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">JavaScript</a></li>
-    </ul>
-  </nav>
+<body class=" indigo lighten-5">
+	<nav>
+		<nav class="nav-extended amber">
+			<div class="nav-wrapper">
+				<ul id="nav-mobile" class="right">
+					<li><a href="Inicial.php" class="red-text text-darken-3" style="font-family: Revalia">Produtos</a></li>
+					<li><a href="Carrinho.php" class="red-text text-darken-3 valign-wrapper" style="font-family: Revalia">Carrinho <i class="material-icons">add_shopping_cart</i></a></li>
+					<li><a href="Usuario.php" class="red-text text-darken-3" style="font-family: Revalia">Registrar-se/entrar</a></li>
+				</ul>
+			</div>
+			<div class="nav-header center">
+				<h1 class=" red-text text-darken-3" style="font-family: Revalia">MotorShaft</h1>
+			</div>
+			<div class="nav-content  red darken-3 ">
+				<ul class="tabs tabs-transparent">
+					<li class="tab"><a href=""><i class="material-icons amber-text">sort</i></a></li>
+				</ul>
+			</div>
+		</nav>
 
+		<div class="row">
+			<div class="col s3">
+				<div class="card Large hoverable">
+					<div class="card-image">
+						<img src="https://cdn.pixabay.com/photo/2017/03/19/18/51/tuning-2157354_960_720.jpg" alt="" />
+						<span class="card-title"></span>
 
-  <div class="row">
-    <div class="col s3">
-      <div class="card medium">
-        <div class="card-image">
-          <img src="" alt="" />
-          <span class="card-title"></span>
-        </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-        </div>
+						<div class="card-content">
+							<div class="row">
+								<p class="black-text valign-wrapper" style="font-family: 'Reem Kufi', sans-serif;font-size:1.7rem">
+									<i class="material-icons amber-text">star</i> Blocos de Motor</p>
 
+								<p class="black-text" style="font-family: reem kufi; font-size:1.2rem">Marcas mais
+									comuns
+									<a class="red-text" href="">Bmw</a>, <a class="red-text" href="">Ferrari</a>
+									e <a class="red-text" href="">Bugatti</a>.
+								</p>
 
-      </div>
+							</div>
+							<div class="card-action">
+								<p class="right-align">
+									<a href="#" style="font-family: reem kufi;" class="valign-wrapper red-text ">Mostrar Mais <i class="material-icons"> description</i></a>
+								</p>
 
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="col s3">
+					<div class="card Large hoverable">
+						<div class="card-image">
+							<img src="https://media.istockphoto.com/photos/turbocharger-structure-scheme-picture-id649330904" alt="" />
+							<span class="card-title"></span>
+
+							<div class="card-content">
+								<div class="row">
+									<p class="black-text valign-wrapper" style="font-family: 'Reem Kufi', sans-serif;font-size:1.7rem">
+										<i class="material-icons amber-text">star</i> Turbo</p>
+
+									<p class="black-text" style="font-family: reem kufi; font-size:1.2rem">Marcas mais
+										comuns
+										<a class="red-text" href="">Bmw</a>, <a class="red-text" href="">Ferrari</a>
+										e <a class="red-text" href="">Bugatti</a>.
+									</p>
+
+								</div>
+								<div class="card-action">
+									<p class="right-align">
+										<a href="#" style="font-family: reem kufi;" class="valign-wrapper red-text ">Mostrar Mais <i class="material-icons"> description</i></a>
+									</p>
+
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="row">
+					<div class="col s3">
+						<div class="card Large hoverable">
+							<div class="card-image">
+								<img src="https://cdn.pixabay.com/photo/2018/01/06/21/57/motor-3066046_960_720.jpg" alt="" />
+								<span class="card-title"></span>
+
+								<div class="card-content">
+									<div class="row">
+										<p class="black-text valign-wrapper" style="font-family: 'Reem Kufi', sans-serif;font-size:1.7rem">
+											<i class="material-icons amber-text">star</i> Filtros de Ar</p>
+
+										<p class="black-text" style="font-family: reem kufi; font-size:1.2rem">Marcas
+											mais comuns
+											<a class="red-text" href="">Bmw</a>, <a class="red-text" href="">Ferrari</a>
+											e <a class="red-text" href="">Bugatti</a>.
+										</p>
+
+									</div>
+									<div class="card-action">
+										<p class="right-align">
+											<a href="#" style="font-family: reem kufi;" class="valign-wrapper red-text ">Mostrar Mais <i class="material-icons"> description</i></a>
+										</p>
+
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="row">
+						<div class="col s3">
+							<div class="card Large hoverable">
+								<div class="card-image">
+									<img src="https://media.istockphoto.com/photos/engine-pistons-crankshaft-mechanism-3d-render-picture-id1059972498" alt="" />
+									<span class="card-title"></span>
+
+									<div class="card-content">
+										<div class="row">
+											<p class="black-text valign-wrapper" style="font-family: 'Reem Kufi', sans-serif;font-size:1.7rem">
+												<i class="material-icons amber-text">star</i> Cilindros</p>
+
+											<p class="black-text" style="font-family: reem kufi; font-size:1.2rem">
+												Marcas mais comuns
+												<a class="red-text" href="">Bmw</a>, <a class="red-text" href="">Ferrari</a>
+												e <a class="red-text" href="">Bugatti</a>.
+											</p>
+
+										</div>
+										<div class="card-action">
+											<p class="right-align">
+												<a href="#" style="font-family: reem kufi;" class="valign-wrapper red-text ">Mostrar Mais <i class="material-icons"> description</i></a>
+											</p>
+
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
 
 </body>
 
